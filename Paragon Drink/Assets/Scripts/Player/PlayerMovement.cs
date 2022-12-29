f622a5ac81b898f14522bd486791497a8ff2d0fb
Minotoror29 <matthieu.boissel@laposte.net>
2022-12-29 22:24:15 +0100

    Update PlayerMovement.cs

--- Paragon Drink/Assets/Scripts/Player/PlayerMovement.cs
+++ Paragon Drink/Assets/Scripts/Player/PlayerMovement.cs
@@ -46,7 +46,7 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-            //Destroy(gameObject);
+            Destroy(gameObject);
         }
 
         Initialize();
