1ac08715456720814562b9ab1967755ba3a3a59f
Minotoror29 <matthieu.boissel@laposte.net>
2023-04-10 16:14:39 +0200

    URP + Collectable Sounds

--- Paragon_Drink/Assets/Scripts/Levels/Objects/Collectible.cs
+++ Paragon_Drink/Assets/Scripts/Levels/Objects/Collectible.cs
@@ -1,3 +1,5 @@
+using FMOD.Studio;
+using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +11,20 @@ public class Collectible : MonoBehaviour
     private Animator _animator;
     private Collider2D _collider;
 
+    private EventInstance _idleSound;
+    private EventInstance _collectSound;
+
     public void Initialize(GameManager gameManager)
     {
         _gameManager = gameManager;
         _animator = GetComponent<Animator>();
         _collider = GetComponent<Collider2D>();
+
+        _idleSound = RuntimeManager.CreateInstance("event:/Objects/collectable_idle");
+        _idleSound.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject));
+        _collectSound = RuntimeManager.CreateInstance("event:/Objects/collectable_collect");
+
+        _idleSound.start();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,6 +40,9 @@ public class Collectible : MonoBehaviour
         _collider.enabled = false;
         _gameManager.CollectItem();
         _animator.CrossFade("Collectable-Explosion", 0);
+
+        _idleSound.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        _collectSound.start();
 
         yield return new WaitForSeconds(0.8f);
 
