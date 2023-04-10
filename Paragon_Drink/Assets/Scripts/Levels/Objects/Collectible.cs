88951dd18cbe8570994afc8afc3a3ef3b26a3e7b
Minotoror29 <matthieu.boissel@laposte.net>
2023-04-10 15:36:29 +0200

    Collectable

--- Paragon_Drink/Assets/Scripts/Levels/Objects/Collectible.cs
+++ Paragon_Drink/Assets/Scripts/Levels/Objects/Collectible.cs
@@ -6,17 +6,32 @@ public class Collectible : MonoBehaviour
 {
     private GameManager _gameManager;
 
+    private Animator _animator;
+    private Collider2D _collider;
+
     public void Initialize(GameManager gameManager)
     {
         _gameManager = gameManager;
+        _animator = GetComponent<Animator>();
+        _collider = GetComponent<Collider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<PlayerController>())
         {
-            _gameManager.CollectItem();
-            gameObject.SetActive(false);
+            StartCoroutine(Collect());
         }
+    }
+
+    private IEnumerator Collect()
+    {
+        _collider.enabled = false;
+        _gameManager.CollectItem();
+        _animator.CrossFade("Collectable-Explosion", 0);
+
+        yield return new WaitForSeconds(0.8f);
+
+        gameObject.SetActive(false);
     }
 }
